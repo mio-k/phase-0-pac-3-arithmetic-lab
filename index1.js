4cd96f1b34913f1fd6e600a9c73d165f7e79f23c
mio kataoka <mio.kanna@gmail.com>
2022-02-10 10:36:36 -0800

    commit tree 4140ba6f6444f46960df2a07a994aca49e6a525d

---
+++ index1.js
@@ -0,0 +1,36 @@
+function add() {
+    return (number += 3);
+}
+function subtract() {
+    return (number -= 6);
+}
+function multiply() {
+    return (number *= 8);
+}
+function divide() {
+    return (number /= 4);
+}
+function add(a,b) {
+    return (a + b);
+}
+function subtract(a,b) {
+    return(a - b);
+}
+function multiply(a,b) {
+    return(a * b);
+}
+function divide(a,b) {
+    return(a / b);
+}
+function increment(n) {
+    return (n += 1);
+}
+function decrement(n) {
+    return (n - 1);
+}
+function makeInt(string) {
+    return parseInt(string, 10);
+}
+function preserveDecimal(string) {
+    return parseFloat(string);
+}
